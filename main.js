076846dfee7a07c25ffd5e8a9b88413fa1b2ad34
mysitego <mahmouddaghbas@gmail.com>
2023-11-23 21:17:52 +0300

    update names

--- main.js
+++ main.js
@@ -11,61 +11,61 @@ document.addEventListener("click", (event) => {
   }
 });
 
-const slider = document.querySelector('.slider');
-const overlay = document.querySelector('.slider-overlay');
-const nameElement = document.querySelector('.name');
+// const slider = document.querySelector('.slider');
+// const overlay = document.querySelector('.slider-overlay');
+// const nameElement = document.querySelector('.name');
 
-let currentIndex = 0;
-const images = slider.querySelectorAll('img');
-const names = [
-  'Raghad Abdulrahman',
-  'Shrog  Al-balawi',
-  'Maymonah Aljammaz',
-  'Afnan Al-anazi'
-];
+// let currentIndex = 0;
+// const images = slider.querySelectorAll('img');
+// const names = [
+//   'Raghad Abdulrahman',
+//   'Shrog  Al-balawi',
+//   'Maymonah Aljammaz',
+//   'Afnan Al-anazi'
+// ];
 
-// تقسيم الأسماء إلى أربعة أسطر
-const rows = [];
-const namesPerRow = 4;
-for (let i = 0; i < names.length; i += namesPerRow) {
-  const row = names.slice(i, i + namesPerRow);
-  rows.push(row);
-}
+// // تقسيم الأسماء إلى أربعة أسطر
+// const rows = [];
+// const namesPerRow = 4;
+// for (let i = 0; i < names.length; i += namesPerRow) {
+//   const row = names.slice(i, i + namesPerRow);
+//   rows.push(row);
+// }
 
-function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
+// function shuffle(array) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [array[i], array[j]] = [array[j], array[i]];
+//   }
+// }
 
-function showOverlay() {
-  overlay.classList.add('show');
-}
+// function showOverlay() {
+//   overlay.classList.add('show');
+// }
 
-function hideOverlay() {
-  overlay.classList.remove('show');
-}
+// function hideOverlay() {
+//   overlay.classList.remove('show');
+// }
 
-function updateSlider() {
-  hideOverlay();
+// function updateSlider() {
+//   hideOverlay();
 
-  shuffle(names); // ترتيب عشوائي للأسماء
+//   shuffle(names); // ترتيب عشوائي للأسماء
 
-  for (let i = 0; i < images.length; i++) {
-    if (i === currentIndex) {
-      images[i].style.display = 'block';
-    } else {
-      images[i].style.display = 'none';
-    }
-  }
+//   for (let i = 0; i < images.length; i++) {
+//     if (i === currentIndex) {
+//       images[i].style.display = 'block';
+//     } else {
+//       images[i].style.display = 'none';
+//     }
+//   }
 
-  const namesForRow = rows[currentIndex % rows.length];
-  nameElement.innerHTML = namesForRow.join('<br>');
+//   const namesForRow = rows[currentIndex % rows.length];
+//   nameElement.innerHTML = namesForRow.join('<br>');
 
-  showOverlay();
+// //   showOverlay();
 
-  currentIndex = (currentIndex + 1) % images.length;
-}
+//   currentIndex = (currentIndex + 1) % images.length;
+// }
 
-document.addEventListener('DOMContentLoaded', updateSlider);
+// updateSlider(); // تحديث السلايدر عند تحميل الصفحة
