43e90f09ac228d75f0b0adef6293acd2ee0dd5f8
mysitego <mahmouddaghbas@gmail.com>
2023-11-23 21:47:55 +0300

    update3

---
+++ main.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+});
+
+document.addEventListener("click", (event) => {
+  if (!event.target.closest("nav") && navLinks.classList.contains("nav-active")) {
+    navLinks.classList.remove("nav-active");
+  }
+});
